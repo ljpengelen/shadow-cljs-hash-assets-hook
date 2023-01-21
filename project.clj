d015702a8989b4669cf82ac07bf0560c4bb9a0ae
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2023-01-21 15:55:35 +0100

    Add missing dependencies

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "Add hashes to the filenames of your static assets and use those in your index.html"
   :url "https://github.com/ljpengelen/shadow-cljs-hash-assets-hook/"
   :license {:name "MIT"
-            :url "https://opensource.org/licenses/MIT"})
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[commons-codec/commons-codec "1.15"]
+                 [org.clojure/clojure "1.11.1"]])
